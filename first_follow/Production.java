c0107451cde1df3b541b2bf6f3fc2cdee15373a4
vinay-deshmukh <vinay_deshmukh@outlook.com>
2019-03-19 18:50:00 +0530

    move grammar,production to separate files

---
+++ first_follow/Production.java
@@ -0,0 +1,20 @@
+package first_follow;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Production{
+    final String head;
+    final List<String> body;
+
+    public Production(String head, String[] body) {
+        this.head = head;
+        this.body = Arrays.asList(body);
+    }
+
+    @Override
+    public String toString() {
+        return head + " -> " + String.join(" ",body);
+    }
+}
+
